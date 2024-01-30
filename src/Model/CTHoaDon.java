544958ac9a215d09df6534616e1b116c0e190fb3
Kiet <git@github.com>
2024-01-30 13:18:46 +0700

    create model hoadon, cthoadon

---
+++ src/Model/CTHoaDon.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Anh Kiet
+ */
+public class CTHoaDon {
+    private int maHoaDon;
+    private String maVatTu;
+    private int soLuong;
+    private float donGia;
+    private float thanhTien;
+
+
+    public CTHoaDon() {
+    }
+
+    public int getMaHoaDon() {
+        return maHoaDon;
+    }
+
+    public void setMaHoaDon(int maHoaDon) {
+        this.maHoaDon = maHoaDon;
+    }
+
+    
+    public String getMaVatTu() {
+        return maVatTu;
+    }
+
+    public void setMaVatTu(String maVatTu) {
+        this.maVatTu = maVatTu;
+    }
+
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public float getDonGia() {
+        return donGia;
+    }
+
+    public void setDonGia(float donGia) {
+        this.donGia = donGia;
+    }
+
+    public float getThanhTien() {
+        return thanhTien;
+    }
+
+    public void setThanhTien(float thanhTien) {
+        this.thanhTien = thanhTien;
+    }
+}
--- src/quanlyvt/Quanlyvt.java
+++ src/quanlyvt/Quanlyvt.java
@@ -14,7 +14,7 @@ public class Quanlyvt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+       
         System.out.println("quanlyvt.Quanlyvt.main()");
         // TODO code application logic here
     }
