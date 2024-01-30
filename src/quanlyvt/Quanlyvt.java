690f66cd7814aba8ce97ea72d026111335b1bd29
Kiet <git@github.com>
2024-01-30 12:24:01 +0700

    create project

--- src/quanlyvt/Quanlyvt.java
+++ src/quanlyvt/Quanlyvt.java
@@ -14,6 +14,8 @@ public class Quanlyvt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("quanlyvt.Quanlyvt.main()");
         // TODO code application logic here
     }
     
